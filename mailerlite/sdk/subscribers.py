28dab534ec34889dd860a53fb560df6a508da1d4
Igor Hrček <igor@hrcek.rs>
2022-12-20 12:25:53 +0100

    feat: Finished implementation of Subscribers API and tests

--- mailerlite/sdk/subscribers.py
+++ mailerlite/sdk/subscribers.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 from mailerlite.api_client import ApiClient
-
+import re
+import json
+import urllib
 
 class Subscribers(object):
     base_api_url = "api/subscribers"
@@ -29,3 +31,79 @@ class Subscribers(object):
             query_params[key] = val
 
         return self.api_client.request("GET", self.base_api_url, query_params).json()
+
+    def create(self, email, **kwargs):
+        available_params = ["fields", "groups", "status", "subscribed_at", "ip_address", "opted_in_at", "optin_ip", "unsubscribed_at"]
+        
+        valid = re.search(r'[\w.]+\@[\w.]+', email)
+
+        if not valid:
+            raise ValueError('Email address is not valid.')
+
+        params = locals()
+        body_params = {
+            'email': email
+        }
+
+        for key, val in params["kwargs"].items():
+            if key not in available_params:
+                raise TypeError("Got an unknown argument '%s'" % key)
+
+            if key == 'fields' and type(val) is not dict:
+                raise TypeError('Fields argument should be a dict.')
+
+            if key == 'groups' and type(val) is not list:
+                raise TypeError('Groups argument should be a list.')
+
+            body_params[key] = val
+        
+        return self.api_client.request("POST", self.base_api_url, body=body_params).json()
+
+    def update(self, email, **kwargs):
+        available_params = ["fields", "groups", "status", "subscribed_at", "ip_address", "opted_in_at", "optin_ip", "unsubscribed_at"]
+        
+        valid = re.search(r'[\w.]+\@[\w.]+', email)
+
+        if not valid:
+            raise ValueError('Email address is not valid.')
+
+        params = locals()
+        body_params = {
+            'email': email
+        }
+
+        for key, val in params["kwargs"].items():
+            if key not in available_params:
+                raise TypeError("Got an unknown argument '%s'" % key)
+
+            if key == 'fields' and type(val) is not dict:
+                raise TypeError('Fields argument should be a dict.')
+
+            if key == 'groups' and type(val) is not list:
+                raise TypeError('Groups argument should be a list.')
+
+            body_params[key] = val
+        
+        return self.api_client.request("POST", self.base_api_url, body=body_params).json()
+    
+    def get(self, id):
+        valid = re.search(r'[\w.]+\@[\w.]+', id)
+
+        if not valid and not isinstance(id, int):
+            raise ValueError('Email address or subscriber id are not valid.')
+
+        return self.api_client.request("GET", '{}/{}'.format(self.base_api_url, id)).json()
+
+    def delete(self, id):
+        if not isinstance(id, int):
+            raise ValueError('Subscriber ID is not valid.')
+
+        response = self.api_client.request("DELETE", '{}/{}'.format(self.base_api_url, id))
+
+        return response.status_code
+
+    def get_import(self, id):
+        if not isinstance(id, int):
+            raise ValueError('Subscriber ID is not valid.')
+
+        return self.api_client.request("GET", '{}/{}/{}'.format(self.base_api_url, 'import', id)).json()
--- tests/subscribers_test.py
+++ tests/subscribers_test.py
@@ -12,6 +12,10 @@ from pprint import pprint
 def subscriber_keys():
     return ['id', 'email', 'status', 'source', 'sent', 'opens_count', 'click_rate', 'ip_address', 'subscribed_at', 'unsubscribed_at', 'created_at', 'updated_at']
 
+@fixture
+def import_keys():
+    return ['id', 'total', 'processed', 'imported', 'updated', 'errored', 'percent', 'done', 'invalid', 'invalid_count', 'mistyped', 'mistyped_count', 'changed', 'changed_count', 'unchanged', 'unchanged_count', 'unsubscribed', 'unsubscribed_count', 'role_based', 'role_based_count', 'banned_import_emails_count', 'updated_at', 'undone_at', 'stopped_at', 'undo_started_at', 'finished_at']
+
 class TestSubscribers:
     @classmethod
     def setup_class(self):
@@ -34,3 +38,74 @@ class TestSubscribers:
         assert isinstance(response['data'], list)
         assert isinstance(response['data'][0], dict)
         assert set(subscriber_keys).issubset(response['data'][0].keys())
+
+    def test_creating_subscriber_will_fail_if_email_address_is_not_valid(self):
+        """Tests validation of parameters when creating a new subscriber"""
+
+        with pytest.raises(ValueError):
+            self.client.subscribers.create('wrong2mail.com')
+
+    def test_creating_subscriber_will_fail_if_unknown_param_is_passed(self):
+        """Tests validation of unsupported parameters when creating a subscriber"""
+        
+        with pytest.raises(TypeError):
+            self.client.subscribers.create('some@email.com', unknownparam=1)
+
+    def test_creating_subscriber_will_fail_if_additional_parameters_arre_not_valid(self):
+        """Tests validation of additional parameters when creating a subscriber"""
+
+        with pytest.raises(TypeError):
+            self.client.subscribers.create('some@email.com', fields=[])
+
+        with pytest.raises(TypeError):
+            self.client.subscribers.create('some@email.com', groups={})
+    
+    @vcr.use_cassette('tests/vcr_cassettes/subscribers-create.yml', remove_headers=['Authorization'])
+    def test_create_subscriber(self, subscriber_keys):
+        """Tests creating new subscriber"""
+        response = self.client.subscribers.create('test5@email.com', fields={'name': 'Igor', 'last_name': 'Something'}, ip_address='1.1.1.1')
+
+        assert isinstance(response, dict)
+        assert isinstance(response['data'], dict)
+        assert set(subscriber_keys).issubset(response['data'].keys())
+
+    @vcr.use_cassette('tests/vcr_cassettes/subscribers-update.yml', remove_headers=['Authorization'])
+    def test_update_subscriber(self, subscriber_keys):
+        response = self.client.subscribers.update('test5@email.com', fields={'name': 'Someone', 'last_name': 'Something'}, ip_address='1.1.1.1')
+
+        assert isinstance(response, dict)
+        assert isinstance(response['data'], dict)
+        assert set(subscriber_keys).issubset(response['data'].keys())
+        assert response['data']['fields']['last_name'] == "Something"
+
+    def test_getting_subscriber_will_fail_if_incorrect_id_is_passed(self):
+        with pytest.raises(ValueError):
+            self.client.subscribers.get('abcdefgh')
+
+    @vcr.use_cassette('tests/vcr_cassettes/subscribers-get.yml', remove_headers=['Authorization'])
+    def test_get_subscriber(self, subscriber_keys):
+        response = self.client.subscribers.get('test5@email.com')
+
+        assert isinstance(response, dict)
+        assert isinstance(response['data'], dict)
+        assert set(subscriber_keys).issubset(response['data'].keys())
+
+    def test_removing_subscriber_will_fail_if_incorrect_id_has_been_passed(self):
+        with pytest.raises(ValueError):
+            self.client.subscribers.get('abcdefgh')
+    
+    @vcr.use_cassette('tests/vcr_cassettes/subscribers-delete.yml', remove_headers=['Authorization'])
+    def test_delete_subscriber(self):
+        response = self.client.subscribers.delete(75007210693855095)
+        assert response == 204
+
+        response = self.client.subscribers.delete(123123)
+        assert response == 404
+    
+    @vcr.use_cassette('tests/vcr_cassettes/subscribers-get-import.yml', remove_headers=['Authorization'])
+    def test_get_subscriber_import(self, import_keys):
+        response = self.client.subscribers.get_import(75009793000998293)
+
+        assert isinstance(response, dict)
+        assert isinstance(response['data'], dict)
+        assert set(import_keys).issubset(response['data'].keys())
