989fd0bb6f59f83b8528e30605135e5bd540ab8e
Igor Hrček <igor@hrcek.rs>
2022-12-21 17:44:35 +0100

    fix: Fixed an issue that prevented filter param to be passed

--- mailerlite/sdk/groups.py
+++ mailerlite/sdk/groups.py
@@ -123,7 +123,11 @@ class Groups(object):
         for key, val in params["kwargs"].items():
             if key not in available_params:
                 raise TypeError("Got an unknown argument '%s'" % key)
-            query_params[key] = val
+            if key == "filter":
+                for filter_key, filter_value in val.items():
+                    query_params[filter_key] = filter_value
+            else:
+                query_params[key] = val
 
         return self.api_client.request(
             "GET",
