833a5b225086e22c9085e358f85dea31668be31c
Igor Hrček <igor@hrcek.rs>
2022-12-21 15:36:22 +0100

    feat: Updated naming convention

--- tests/automations_test.py
+++ tests/automations_test.py
@@ -21,14 +21,12 @@ class TestAutomations:
         self.client = MailerLite.Client({
             'api_key': os.getenv('API_KEY')
         })
-    
+
     def test_api_url_is_properly_set(self):
         assert self.client.automations.base_api_url == "api/automations"
 
     @vcr.use_cassette('tests/vcr_cassettes/automations-list.yml', filter_headers=['Authorization'])
-    def test_list_all_automations(self, automation_keys):
-        """Tests an API call for getting information about all automations"""
-        
+    def test_list_of_all_automations_should_be_returned(self, automation_keys):
         response = self.client.automations.list()
 
         assert isinstance(response, dict)
@@ -37,9 +35,7 @@ class TestAutomations:
         assert set(automation_keys).issubset(response['data'][0].keys())
 
     @vcr.use_cassette('tests/vcr_cassettes/automations-get.yml', filter_headers=['Authorization'])
-    def test_get_automation(self, automation_keys):
-        """Tests an API call for getting information about one automation"""
-        
+    def test_automation_information_should_be_returned_when_valid_automation_id_is_provided(self, automation_keys):
         response = self.client.automations.get(75040845299975641)
 
         assert isinstance(response, dict)
@@ -47,9 +43,7 @@ class TestAutomations:
         assert set(automation_keys).issubset(response['data'].keys())
 
     @vcr.use_cassette('tests/vcr_cassettes/automations-activity.yml', filter_headers=['Authorization'])
-    def test_get_automation_activity(self, automation_keys):
-        """Tests an API call for getting activity information of automation"""
-        
+    def test_automation_activity_information_should_be_returned_when_valid_automation_id_is_provided(self):
         response = self.client.automations.activity(75040845299975641, filter={"filter[status]": "active"})
 
         assert isinstance(response, dict)
