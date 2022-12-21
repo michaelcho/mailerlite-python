890ecd268429e7f1342f4171a2983ca5c661117c
Igor Hrček <igor@hrcek.rs>
2022-12-21 14:51:45 +0100

    docs: Updated documentation

--- mailerlite/sdk/automations.py
+++ mailerlite/sdk/automations.py
@@ -1,27 +1,55 @@
 from __future__ import absolute_import
-from mailerlite.api_client import ApiClient
-import json
 
 
 class Automations(object):
+    """
+    Automations base API uri
+    """
     base_api_url = "api/automations"
 
     def __init__(self, api_client):
+        """
+        Class constructor
+
+        :param api_client: object
+        """
         self.api_client = api_client
 
-    def list(self):
+    def list(self, **kwargs):
         """
-        List all automations
+        Lists all automations on account
+        Ref: https://developers.mailerlite.com/docs/automations.html#list-all-automations
+
+        :param **kwargs: You can pass additional arguments - page, limit or to filter by status, name and group
+        :return: JSON array
+        :rtype: dict
         """
 
         return self.api_client.request("GET", self.base_api_url).json()
 
     def get(self, automation_id):
+        """
+        Returns information about single automation
+        Ref: https://developers.mailerlite.com/docs/automations.html#get-an-automation
+
+        :param: automation_id: Automation ID
+        :return: JSON dict
+        """
+
         return self.api_client.request(
             "GET", "{}/{}".format(self.base_api_url, automation_id)
         ).json()
 
     def activity(self, automation_id, **kwargs):
+        """
+        Get the subscriber activity for an automation
+        Ref: https://developers.mailerlite.com/docs/automations.html#get-the-subscriber-activity-for-an-automation
+
+        :param: automation_id: Automation ID
+        :param: kwargs: You can pass additional arguments - page, limit or to filter by status, date_from, date_to
+        :raises: :class: `TypeError` : Got an unknown argument
+        :return: JSON dict
+        """
 
         available_params = ["filter", "page", "limit"]
 
