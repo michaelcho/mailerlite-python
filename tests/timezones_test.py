97a3d6db968549fc04fb240c5a56eece96e49f18
Igor Hrček <igor@hrcek.rs>
2022-12-21 12:55:41 +0100

    fix: Fixed incorrect header filtration

--- tests/timezones_test.py
+++ tests/timezones_test.py
@@ -6,6 +6,7 @@ import mailerlite as MailerLite
 from dotenv import load_dotenv
 from pytest import fixture
 
+
 @fixture
 def timezone_keys():
     return ['id', 'name', 'name_for_humans', 'offset_name', 'offset']
@@ -19,11 +20,11 @@ class TestTimezones:
         self.client = MailerLite.Client({
             'api_key': os.getenv('API_KEY')
         })
-    
+
     def test_api_url_is_properly_set(self):
         assert self.client.timezones.base_api_url == "api/timezones"
 
-    @vcr.use_cassette('tests/vcr_cassettes/timezones-list.yml', remove_headers=['Authorization'])
+    @vcr.use_cassette('tests/vcr_cassettes/timezones-list.yml', filter_headers=['Authorization'])
     def test_list_all_timezones(self, timezone_keys):
         response = self.client.timezones.list()
 
