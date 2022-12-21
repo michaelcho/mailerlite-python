935882ab2d506355bcbed1c59054c1211d0872e1
Igor Hrček <igor@hrcek.rs>
2022-12-21 15:38:40 +0100

    feat: Updated naming convention

--- tests/automations_test.py
+++ tests/automations_test.py
@@ -14,6 +14,9 @@ def automation_keys():
 
 class TestAutomations:
 
+    # Automationd ID used in tests
+    automation_id = 75040845299975641
+
     @classmethod
     def setup_class(self):
         load_dotenv()
@@ -36,7 +39,7 @@ class TestAutomations:
 
     @vcr.use_cassette('tests/vcr_cassettes/automations-get.yml', filter_headers=['Authorization'])
     def test_automation_information_should_be_returned_when_valid_automation_id_is_provided(self, automation_keys):
-        response = self.client.automations.get(75040845299975641)
+        response = self.client.automations.get(self.automation_id)
 
         assert isinstance(response, dict)
         assert isinstance(response['data'], dict)
@@ -44,7 +47,7 @@ class TestAutomations:
 
     @vcr.use_cassette('tests/vcr_cassettes/automations-activity.yml', filter_headers=['Authorization'])
     def test_automation_activity_information_should_be_returned_when_valid_automation_id_is_provided(self):
-        response = self.client.automations.activity(75040845299975641, filter={"filter[status]": "active"})
+        response = self.client.automations.activity(self.automation_id, filter={"filter[status]": "active"})
 
         assert isinstance(response, dict)
         assert isinstance(response['data'], list)
